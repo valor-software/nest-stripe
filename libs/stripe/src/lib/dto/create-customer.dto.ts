1c625695dda73cfd92beddc65d5b5e7204200aad
StashBank <opavlovskyi@gmail.com>
2022-07-18 15:51:15 +0300

    feat: create customer dto

--- libs/stripe/src/lib/dto/create-customer.dto.ts
+++ libs/stripe/src/lib/dto/create-customer.dto.ts
@@ -1,5 +1,25 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsArray, IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class Shipping {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @ApiProperty()
+  @IsString()
+  addressLine2: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  phone?: string;
+}
 
 export class CreateCustomerDto {
   @ApiProperty()
@@ -7,7 +27,64 @@ export class CreateCustomerDto {
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiPropertyOptional()
   @IsString()
   name: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  description: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  addressLine1?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  addressLine2?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  phone?: string;;
+
+  @ApiPropertyOptional({
+    description: 'The prefix for the customer used to generate unique invoice numbers. Must be 3–12 uppercase letters or numbers.'
+  })
+  @IsString()
+  @MinLength(3)
+  @MaxLength(12)
+  invoicePrefix?: string;
+
+  @ApiPropertyOptional()
+  metadata: {[name: string]: string |  number | null};
+
+  @ApiPropertyOptional()
+  @IsString()
+  paymentMethod?: string;
+
+  @ApiPropertyOptional({
+    type: 'string',
+    isArray: true
+  })
+  @IsArray()
+  preferredLocales?: Array<string>;
+
+  @ApiPropertyOptional({
+    description: 'The API ID of a promotion code to apply to the customer. The customer will have a discount applied on all recurring payments. Charges you create through the API will not have the discount.'
+  })
+  @IsString()
+  promotionCode?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  source?: string;
+
+  @ApiPropertyOptional({ type: Shipping })
+  shipping?: Shipping;
+  @ApiPropertyOptional({
+    enum: ['exempt', 'none', 'reverse'],
+    default: 'none' ,
+    description: `The customer's tax exemption. One of \`none\`, \`exempt\`, or \`reverse\`.`
+  })
+  taxExempt?: 'exempt' | 'none' | 'reverse';
 }
