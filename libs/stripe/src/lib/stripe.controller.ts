80cf86f72ae264f6043fa5b37cdaaf6ca0121964
StashBank <opavlovskyi@gmail.com>
2022-07-20 16:34:58 +0300

    refactor: API refactor

--- libs/stripe/src/lib/stripe.controller.ts
+++ libs/stripe/src/lib/stripe.controller.ts
@@ -32,36 +32,49 @@ export class StripeController {
   constructor(private stripeService: StripeService) {}
 
   @ApiResponse({ type: CheckoutSessionResponse })
-  @Post('/create-checkout-session')
+  @ApiTags('Checkout Session')
+  @Post('/checkout-session/create')
   createCheckoutSession(@Body() dto: CreateCheckoutSessionDto): Promise<CheckoutSessionResponse> {
     return this.stripeService.createCheckoutSession(dto);
   }
 
   @ApiResponse({ type: CustomerResponse })
-  @Post('/create-customer')
+  @ApiTags('Customer')
+  @Post('/customer/create')
   createCustomer(@Body() dto: CreateCustomerDto): Promise<CustomerResponse> {
     return this.stripeService.createCustomer(dto);
   }
 
   @ApiResponse({ type: CustomerResponse })
+  @ApiTags('Customer')
   @Post('/customer/:customerId/attach-payment-method/:paymentMethodId')
   attachPaymentMethod(@Param('customerId') customerId: string, @Param('paymentMethodId') paymentMethodId: string): Promise<CustomerResponse> {
     return this.stripeService.attachPaymentMethod(paymentMethodId, customerId);
   }
 
+  @ApiResponse({ type: SubscriptionsResponse })
+  @ApiTags('Customer', 'Subscription')
+  @Get('/customer/:customerId/subscriptions')
+  customerSubscriptions(@Param('customerId') customerId: string): Promise<SubscriptionsResponse> {
+    return this.stripeService.customerSubscriptions(customerId);
+  }
+
   @ApiResponse({ type: CustomerResponse })
+  @ApiTags('Payment Method')
   @Post('/payment-method/:paymentMethodId/detach')
   detachPaymentMethod(@Param('paymentMethodId') paymentMethodId: string): Promise<CustomerResponse> {
     return this.stripeService.detachPaymentMethod(paymentMethodId);
   }
 
   @ApiResponse({ type: CreatePaymentMethodResponse })
-  @Post('/create-payment-method')
+  @ApiTags('Payment Method')
+  @Post('/payment-method/create')
   createPaymentMethod(@Body() dto: CreatePaymentMethodDto): Promise<CreatePaymentMethodResponse> {
     return this.stripeService.createPaymentMethod(dto);
   }
 
   @ApiResponse({ type: BaseDataResponse })
+  @ApiTags('Payment Method')
   @ApiQuery({
     name: 'type',
     enum: PaymentMethodTypes
@@ -71,43 +84,42 @@ export class StripeController {
     type: 'string',
     required: false
   })
-  @Post('/paymentMethods')
+  @Post('/payment-method-list')
   paymentMethodList(@Query('type') type: any, @Query('customerId') customerId: string): Promise<BaseDataResponse<any[]>> {
     return this.stripeService.paymentMethodList(type, customerId);
   }
 
   @ApiResponse({ type: PriceResponse })
-  @Post('/create-price')
+  @ApiTags('Price')
+  @Post('/price/create')
   createPrice(@Body() dto: CreatePriceDto): Promise<PriceResponse> {
     return this.stripeService.createPrice(dto);
   }
 
   @ApiResponse({ type: SubscriptionResponse })
-  @Post('/create-subscription')
+  @ApiTags('Subscription')
+  @Post('/subscription/create')
   createSubscription(@Body() dto: CreateSubscriptionDto): Promise<SubscriptionResponse> {
     return this.stripeService.createSubscription(dto);
   }
 
-  @ApiResponse({ type: SubscriptionsResponse })
-  @Get('/customer/:customerId/subscriptions')
-  customerSubscriptions(@Param('customerId') customerId: string): Promise<SubscriptionsResponse> {
-    return this.stripeService.customerSubscriptions(customerId);
-  }
-
   @ApiResponse({ type: SubscriptionResponse })
-  @Post('/cancel-subscription')
-  cancelSubscription(@Body() dto: CancelSubscriptionDto): Promise<SubscriptionResponse> {
-    return this.stripeService.cancelSubscription(dto);
+  @ApiTags('Subscription')
+  @Post('/subscription/:subscriptionId/cancel')
+  cancelSubscription(@Param() subscriptionId: string): Promise<SubscriptionResponse> {
+    return this.stripeService.cancelSubscription({ subscriptionId });
   }
 
   @ApiResponse({ type: InvoicePreviewResponse })
+  @ApiTags('Invoice')
   @Post('/upcoming-invoice-preview')
   upcomingInvoicePreview(@Body() dto: InvoicePreviewDto): Promise<InvoicePreviewResponse> {
     return this.stripeService.upcomingInvoicePreview(dto);
   }
 
   @ApiResponse({ type: CreateUsageRecordResponse })
-  @Post('/create-usage-record/:subscriptionItemId')
+  @ApiTags('Usage Record')
+  @Post('/usage-record/create/:subscriptionItemId')
   createUsageRecord(@Param('subscriptionItemId') subscriptionItemId: string, @Body() dto: CreateUsageRecordDto): Promise<CreateUsageRecordResponse> {
     return this.stripeService.createUsageRecord(subscriptionItemId, dto);
   }
