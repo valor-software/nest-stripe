af5791875b2fcb65d1957d7300e9982c7a15b741
StashBank <opavlovskyi@gmail.com>
2022-08-15 16:50:40 +0300

    usage record

--- apps/nest-stripe-test/src/app/app.service.ts
+++ apps/nest-stripe-test/src/app/app.service.ts
@@ -1,10 +1,16 @@
-import { WebhookEventType, WebhookService } from '@nest/stripe';
-import { Injectable } from '@nestjs/common';
+import { StripeService, WebhookEventType, WebhookService } from '@nest/stripe';
+import { Injectable, Logger } from '@nestjs/common';
+import Stripe from 'stripe';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly stripeWebhookService: WebhookService) {
-    this.stripeWebhookService.subscribeToEvent(WebhookEventType.invoicePaid).subscribe(console.log)
+  constructor(
+    private readonly stripeWebhookService: WebhookService,
+    private readonly stripeService: StripeService
+  ) {
+    this.stripeWebhookService.subscribeToEvent(WebhookEventType.invoicePaid).subscribe(console.log);
+    this.stripeWebhookService.subscribeToEvent(WebhookEventType.customerSubscriptionCreated).subscribe((e) => this.createMeteredUsage(e));
+    this.stripeWebhookService.subscribeToEvent(WebhookEventType.customerSubscriptionUpdated).subscribe((e) => this.createMeteredUsage(e));
   }
 
   getConfig(): any {
@@ -12,4 +18,30 @@ export class AppService {
       stripePublicKey: process.env.STRIPE_PUBLIC_KEY
     };
   }
+
+  async createMeteredUsage(evt: Stripe.Event) {
+    const stripeSubscription = evt.data.object as Stripe.Subscription;
+    const subscriptionRes = await this.stripeService.getSubscriptionById(stripeSubscription.id);
+    if (subscriptionRes.success) {
+      const subscription = subscriptionRes.data;
+      subscription.items.forEach(async si => {
+        if (si.plan.usageType === 'metered') {
+          const existingRes = await this.stripeService.listUsageRecordSummaries(si.id);
+          const r = await this.stripeService.createUsageRecord(si.id, {
+            quantity: si.quantity || 1,
+            action: 'increment',
+            timestamp: 'now',
+          });
+          if (!r.success) {
+            Logger.error(r.errorMessage, 'Create Usage Record');
+          } else {
+            Logger.debug(r.usageRecord.id, 'Usage Record')
+          }
+        }
+      })
+    } else {
+      throw new Error(subscriptionRes.errorMessage);
+    }
+  }
+
 }
