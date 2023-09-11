204ddf1950edd92a3c66bbb0dea5d67787e05fce
Oleksandr Pavlovskyi <opavlovskyi@gmail.com>
2023-09-11 13:23:38 +0300

    fix: remove list params validations

--- libs/stripe/src/lib/dto/list-request-params.dto.ts
+++ libs/stripe/src/lib/dto/list-request-params.dto.ts
@@ -1,20 +1,13 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class ListRequestParamsDto {
   @ApiPropertyOptional()
-  @IsOptional()
-  @IsNumber()
   limit?: number;
 
   @ApiPropertyOptional()
-  @IsOptional()
-  @IsString()
   startingAfter?: string;
 
   @ApiPropertyOptional()
-  @IsOptional()
-  @IsString()
   endingBefore?: string;
 
 }
