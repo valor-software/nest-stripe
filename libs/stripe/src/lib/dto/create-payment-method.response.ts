2b44d9aa329cfaddf6ebbf3d96672f0840c3a56a
Arthur Fiorette <47537704+arthurfiorette@users.noreply.github.com>
2024-12-25 23:52:18 -0300

    Update create-payment-method.response.ts

--- libs/stripe/src/lib/dto/create-payment-method.response.ts
+++ libs/stripe/src/lib/dto/create-payment-method.response.ts
@@ -1,5 +1,7 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { BaseResponse } from './base.response';
 
 export class CreatePaymentMethodResponse extends BaseResponse {
+  @ApiPropertyOptional()
   paymentMethodId?: string;
 }
